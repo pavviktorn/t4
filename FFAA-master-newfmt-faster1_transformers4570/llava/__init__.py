578507d78f02231cc4823ea2b6b33c302672fdd0
pavviktorn <pavviktorn@gmail.com>
2026-09-11 21:16:09 +0000

    Add FFAA-master-newfmt-faster1_transformers4570/llava/__init__.py

---
+++ FFAA-master-newfmt-faster1_transformers4570/llava/__init__.py
@@ -0,0 +1,4 @@
+# from .model import LlavaLlamaForCausalLM
+from .model.language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
+from .model.language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
+from .model.language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
